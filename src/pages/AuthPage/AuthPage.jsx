bceb9b2cb53d7b244a53b6cf58a540b8c080d0bd
btlenny <btlenny@gmail.com>
2023-12-08 07:44:17 -0800

    last changes

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -14,6 +14,9 @@ export default function AuthPage({ setUser }) {
         ) : (
           <LoginForm setUser={setUser} />
         )}
+<div className="flex justify-center items-center my-2">
+          <span className="text-gray-900 font-semibold mx-2">- or -</span>
+        </div>
         <button
           type="submit"
           className="flex mx-auto justify-center rounded-md bg-emerald-300 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-sky-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
